6703887b25aee8f707ecee67e3ba6a6c6a52708d
Hayato <hayato.314.nico@gmail.com>
2023-05-24 00:26:56 +0900

    DB マイグレーションの作成

--- model/task.go
+++ model/task.go
@@ -3,10 +3,17 @@ package model
 import "time"
 
 type Task struct {
-	ID			unit 		`json:"id" gorm:"primaryKey"`
-	Titles		string 		`json:"title" gorm:"not null"`
+	ID			uint 		`json:"id" gorm:"primaryKey"`
+	Title		string 		`json:"title" gorm:"not null"`
 	CreatedAt	time.Time 	`json:"created_at"`
 	UpdatedAt	time.Time 	`json:"updated_at"`
 	User		User 		`json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
-	UserId		unit		`json:"user_id" gorm:"not null"`
+	UserId		uint		`json:"user_id" gorm:"not null"`
+}
+
+type TaskResponse struct {
+	ID			uint		`json:"id"	gorm:"primaryKey"`
+	Title		string 		`json:"title" gorm:"not null"`
+	CreatedAt	time.Time 	`json:"created_at"`
+	UpdatedAt	time.Time 	`json:"updated_at"`
 }
